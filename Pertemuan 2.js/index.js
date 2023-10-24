0f469fa5445fd0e8509a9fde509a4df37aac774f
unknown <arfelynfk@gmail.com>
2023-10-24 16:04:46 +0700

    first commit

---
+++ Pertemuan 2.js/index.js
@@ -0,0 +1,13 @@
+let x = 5;
+let y = 6;
+let z = x + y;
+console.log(z)
+
+const nama="Ofika Parsyanda";
+const prodi="Sistem Informasi";
+const univ="Universitas Metamedia";
+const bp=235720111031;
+console.log(nama);
+console.log(prodi);
+console.log(univ);
+console.log(bp);
---
+++ Pertemuan 2.js/variabel (tempat penyimpanan velue).js
@@ -0,0 +1,10 @@
+variabel (tempat penyimpanan velue)
+var=public
+let (bisa diubah)=yang digunakan bnyk kode
+const  (tidak bisa diubah)
+contoh const nama'fika'
+
+let x = 5;
+let y = 6;
+let z = x + y;
+console.log(z)
