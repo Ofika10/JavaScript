359978713a47f3fd290e1c413980593355b74deb
Ofika10 <arfelynfk@gmail.com>
2023-11-23 13:57:03 +0700

    tugas

--- Pertemuan 2.js/variabel (tempat penyimpanan velue).js
+++ Pertemuan 2.js/variabel (tempat penyimpanan velue).js
@@ -1,8 +1,14 @@
-variabel (tempat penyimpanan velue)
-var=public
+variabel (tempat penyimpanan velue/nilai)
+var=public (awal penciptaannya js)
 let (bisa diubah)=yang digunakan bnyk kode
 const  (tidak bisa diubah)
 contoh const nama'fika'
+tipe data (format penyimpanan data)
+boolean (menentukan false atau true) 
+Objek (Dari kurung kurawal sampe akhir)
+automatically(tidak bisa membuat sintak variabel)
+W3 school (belajar berbagai bahasa pemograman)
+operator pertemuan selanjutnya
 
 let x = 5;
 let y = 6;
---
+++ Pertemuan 3.js/index.js
@@ -0,0 +1,13 @@
+mahasiswa={
+    nama :'ofika',
+    kelas :'Si B',
+    nim :2324
+}
+console.log(mahasiswa.kelas)
+
+function myFunction(a, b){
+    var total;
+    total = a * b;
+    return total;
+}
+console.log(myFunction(2,2));
