d0edde6454aa8ce0c5d9722c696d19065043ac7e
Ofika10 <arfelynfk@gmail.com>
2023-12-06 21:35:13 +0700

    Tugas2

---
+++ Pertemuan 8.js/catatan.ts
@@ -0,0 +1,11 @@
+array sebuah variabel yang terdapat sebuah index
+tipe data yg digunakan untuk mendeskripsikan kumpulan elemen (nilai atau variabel) yang tiap elemennya  memiliki index
+index dimukai dari 0,1,2,3,4
+elemen dimulai dari 1,2,3,4
+boolean (false)
+length = banyak elemen atau menghitung jumlah elemen
+meghapus elemen dalam indek (undefined)(contoh : arr[1]=undefined)
+//let arr3=['senin','selasa','rabu']
+//for(1=0;i<3;i++){
+    //console.log(arr3[i])
+//}
--- Tugas/tugas.js
+++ Tugas/tugas.js
@@ -1,22 +1,17 @@
-<<<<<<< HEAD
-const prompt=require("prompt-sync")({sigint:true});
-let nama = prompt("Masukkan Nama Anda : ");
+let nama = "Salma";
 
-=======
-let nama = prompt("Masukkan Nama Anda");
->>>>>>> b7e8c70a078413ebdeeaf9c85cfd867a7eecf291
 if (nama == "Salma") {
-  alert(
+  console.log(
     "Halo " +
       nama +
       " , anda mengambil kelas Javascript. Kelas anda berada di labor 103, kelas dimulai jam 10.00"
   );
 } else if (nama == "Andre") {
-  alert(
+  console.log(
     "Halo " +
       nama +
       ", anda mengambil kelas Design Grafis. Kelas anda berada di labor 101, kelas dimulai jam 13.00"
   );
 } else {
-  alert("Maaf " + nama + " , anda belum mengambil kelas di Kafekoding");
+  console.log("Maaf " + nama + " , anda belum mengambil kelas di Kafekoding");
 }
