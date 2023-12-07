75c1f9f7fa6c98af319e306a75431f3a42ca9a70
Ofika10 <arfelynfk@gmail.com>
2023-12-07 14:20:28 +0700

    Tugas 2

--- Tugas 2/tugas2.js
+++
@@ -1,4 +0,0 @@
-let alamat =['Arab','Turki','Indonesia',['Aceh','Sumatera Utara','Sumatera Selatan','Jawa Barat','Sumatera Barat',['Agam','Solok','Tanah Datar','Pasaman','Padang Pariaman',['Enam Lingkung','Sintoga','Lubuk Alung',['Balah Hilia','Sikabu Lubuk Alung']]]]]
-
-console.log(alamat[3][5][4][3][1])
-console.log(`Nama saya Ofika yang beralamat di Negara ${alamat[2]}, bertempat di Provinsi ${alamat[3][4]}, di Kabupaten ${alamat[3][5][4]}, lebih tepatnya di Kecamatan ${alamat[3][5][5][2]}, dengan Jorong yang namanya, Jorong ${alamat[3][5][5][3][1]}.`)
