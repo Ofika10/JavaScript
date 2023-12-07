964b8d9ffb4c89c83e2602a82effdb075e0a9916
Ofika10 <arfelynfk@gmail.com>
2023-12-07 14:31:52 +0700

    tugas 2

---
+++ Pertemuan 5 js/operator.js
@@ -0,0 +1,24 @@
+let a= 10
+let b = 5
+
+//console.log("penjumlahan",a + b)
+//console.log("pengurangan",a - b)
+
+
+// 2 pangkat 3
+let angka = 2
+angka **= 3
+
+//console.log(angka)
+
+//Hasil Dari Penugasan Kelas, Javascript
+let hasil = "Kelas " 
+    hasil += "JavaScript"  
+
+//console.log(hasil)
+
+let nilai1 = "80"
+let nilai2 = 80
+
+console.log(nilai1 == nilai2)
+console.log(nilai1 === nilai2)
