860daca23af366966c26889348e71610c8e96066
Ofika10 <arfelynfk@gmail.com>
2023-12-28 19:35:10 +0700

    Tugas Pertemuan 10

---
+++ Pertemuan 10.js/index.js
@@ -0,0 +1,38 @@
+const mahasiswa ={
+    afdal:{
+        no_hp:'082170131713',
+        kelas: '4 SI a',
+        alamat: {
+            jln:"jln cendrawasih no 34",
+            kecamatan:'lembah gumanti'
+        }
+    }
+}
+
+console.log(mahasiswa)
+
+
+
+function jumlahBilangan(a, b){
+    var total
+    total = a + b
+
+    return total
+}
+console.log(jumlahBilangan(10, 30))
+
+
+
+const kelilingSegitiga = (a,b,c)=>{
+
+    let hasil
+    hasil = a*b*c
+
+    return hasil
+}
+
+console.log(kelilingSegitiga(2,2,2))
+
+
+
+ 
